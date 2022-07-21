797c0279d028a7b6afbb1ffab1c3aa16fa872964
petrosh <github@petrosh.de>
2022-07-21 11:02:54 +0200

    add search

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,48 @@
 import { graphql, Link, navigate } from "gatsby"
-import React from "react"
+import React, { useState } from "react"
 import { Router } from "@reach/router"
-import SheetPage from "../templates/sheet-details"
+import SheetPage from "../util/sheet-details"
+import { match } from "../util/util"
 
-const Home = ({sheetlist}) => {
+const Home = ({sheetlist, searchString}) => {
+  if (searchString !== "") {
+    sheetlist.sort((a,b) => {return match(b.frontmatter, searchString) - match(a.frontmatter, searchString)})
+  }
   return (
     <div>
-      {sheetlist.map(sheet => (
-        <Link to={`/sheets/${sheet.frontmatter.slug}`} key={sheet.id}>
-          <div>
-            <h3>{sheet.frontmatter.title}</h3>
-            <p>{sheet.frontmatter.band}</p>
-          </div>
-        </Link>
-      ))}
+      {sheetlist.map(sheet => {
+        if (searchString === "" || match(sheet.frontmatter, searchString) >= 0.5) {
+          return (
+            <Link to={`/sheets/${sheet.frontmatter.slug}`} key={sheet.id}>
+              <div>
+                <h3>{sheet.frontmatter.title}</h3>
+                <p>{sheet.frontmatter.band}</p>
+              </div>
+            </Link>
+          )
+        } else {
+          return (<></>)
+        }
+      })}
     </div>
   )
 }
 
 // TODO: Loading all pages at once can perform badly for huge libraries, fix with LazyLoad vs offline mode?
 export default function Sheetlist({ data }) {
+  const [searchString, setSearchString] = useState("")
+
   const sheetlist = data.allMarkdownRemark.nodes
   sheetlist.sort((a,b) => a.frontmatter.title.localeCompare(b.frontmatter.title))
   const NotFound = () => (<div onLoad={navigate("/", {replace: true})}>Page not found</div>)
+
+
   return (
     <>
       <Link to="/">Home</Link>
+      <input onChange={(event) => {setSearchString(event.target.value)}} />
       <Router>
-        <Home sheetlist={sheetlist} path="/" />
+        <Home sheetlist={sheetlist} searchString={searchString} path="/" />
         {sheetlist.map(sheet => (
           <SheetPage data={sheet} path={`/sheets/${sheet.frontmatter.slug}`} />
         ))}
---
+++ src/util/util.js
@@ -0,0 +1,43 @@
+
+function stringSimilarity(str1, str2, gramSize = 2) {
+  function getNGrams(s, len) {
+    s = ' '.repeat(len - 1) + s.toLowerCase() + ' '.repeat(len - 1);
+    let v = new Array(s.length - len + 1);
+    for (let i = 0; i < v.length; i++) {
+      v[i] = s.slice(i, i + len);
+    }
+    return v;
+  }
+
+  if (!str1?.length || !str2?.length) { return 0.0; }
+
+  let s1 = str1.length < str2.length ? str1 : str2;
+  let s2 = str1.length < str2.length ? str2 : str1;
+
+  let pairs1 = getNGrams(s1, gramSize);
+  let pairs2 = getNGrams(s2, gramSize);
+  let set = new Set(pairs1);
+
+  let total = pairs1.length;
+  let hits = 0;
+  for (let item of pairs2) {
+    if (set.delete(item)) {
+      hits++;
+    }
+  }
+
+  // more weight for prefixes
+  if (s2.toLowerCase().startsWith(s1.toLowerCase())) {
+    hits += s1.length
+  }
+
+  return hits / total;
+}
+
+export function match(data, searchString) {
+  let max = 0;
+  for(const target of [data.title, data.band, data.tags]) {
+    max = Math.max(stringSimilarity(searchString, target), max)
+  }
+  return max
+}

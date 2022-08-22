af50e0d87e161e3477969d371ce884d19c5a1d01
petrosh <github@petrosh.de>
2022-08-23 00:09:14 +0200

    dynamically load icons from config
    some cleanup, slightly better search for longer strings

--- src/components/home.js
+++ src/components/home.js
@@ -1,16 +1,78 @@
 import { Link } from "gatsby"
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { match } from "../util/util"
 import * as styles from '../styles/home.module.css'
 import Magnifier from '../../assets/magnifier.svg'
-import Tags from "../components/tags"
 import Head from "./head"
+import AssetConfig from '../../assets/assets.yml'
 
 
-export default function Home({sheetlist, searchString, setSearchString}) {
+export default function Home({sheetlist}) {
+  const [searchString, setSearchString] = useState("");
   if (searchString !== "") {
-    sheetlist.sort((a,b) => {return match(b.frontmatter, searchString) - match(a.frontmatter, searchString)})
+    sheetlist.sort((a,b) => {return match(b.frontmatter, searchString) - match(a.frontmatter, searchString)});
+  } else {
+    sheetlist.sort((a,b) => a.frontmatter.title.localeCompare(b.frontmatter.title));
   }
+
+  // ---------------------- Icon buffer to avoid loading each icon multiple times
+  const [iconBuffer, setIconBuffer] = useState([]);
+  const [loadingIcons, setLoadingIcons] = useState(false);
+  const [loadingIconsCounter, setLoadingIconsCounter] = useState(0);
+
+  useEffect(() => {
+    setLoadingIcons(true);
+    const conf = AssetConfig.icons;
+    conf.forEach(element => {
+      setLoadingIcons(true);
+      setLoadingIconsCounter(loadingIconsCounter + 1);
+      const importIcon = async () => {
+        try {
+          const { default: namedImport } = await import(`../../assets/${element.path}`);
+          setIconBuffer(oldBuffer => [...oldBuffer, {...element, svg: namedImport}]);
+        } catch (err) {
+          throw err;
+        } finally {
+          setLoadingIconsCounter(loadingIconsCounter - 1);
+          if (loadingIconsCounter === 0) {
+            setLoadingIcons(false);
+          }
+        }
+      };
+      importIcon();
+    })
+  }, []);
+
+  /**
+   * Apply icons to the tags
+   * @param {string} tags
+   * @returns
+   */
+  function iconifyTags(tags) {
+    const allTags = tags.replace(", ", ",").split(",");
+    return (
+      <>
+        {allTags.map((tag) => {
+          const iconConf = iconBuffer.find((i) =>
+            i.tags.includes(tag) || (i.arguments && i.tags.some((t) => tag.startsWith(t))));
+          if (!iconConf) {
+            return tag;
+          }
+          if (!loadingIcons) {
+            const { svg: ImportedIcon } = iconConf;
+            return (
+              <span title={iconConf.name}>
+                <ImportedIcon className={styles.icon} />
+                {iconConf.arguments && iconConf.tags.reduce((pre, cur) => pre.replace(cur, ""), tag)}
+              </span>
+            );
+          }
+          return "";
+        })}
+      </>
+    );
+  }
+
   return (
     <>
       <Head />
@@ -21,16 +83,14 @@ export default function Home({sheetlist, searchString, setSearchString}) {
       <h1 style={{textAlign: "center", margin: "0px"}}>Delyrium</h1>
       <div className={styles.cardbox}>
         {sheetlist.map(sheet => {
-          if (searchString === "" || match(sheet.frontmatter, searchString) >= 0.5) {
+          if (searchString === "" || match(sheet.frontmatter, searchString) >= 0.4 + searchString.length * 0.02) {
             return (
-              <Link to={`/sheets/${sheet.frontmatter.slug}`} key={sheet.id} className={styles.card}>
-                <div>
+              <Link to={`/sheets/${sheet.frontmatter.slug}`} key={sheet.id} className={styles.card} onClick={() => setSearchString("")}>
                   <h3 className={styles.title}>{sheet.frontmatter.title}</h3>
                   <div className={styles.band}>
                     {sheet.frontmatter.band}
-                    <Tags tags={sheet.frontmatter.tags ?? ""} />
+                    {iconifyTags(sheet.frontmatter.tags ?? "")}
                   </div>
-                </div>
               </Link>
             )
           } else {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
-import { graphql, Link, navigate } from "gatsby"
-import React, { useState } from "react"
+import { graphql, navigate } from "gatsby"
+import React from "react"
 import { Router } from "@reach/router"
 import SheetPage from "../components/sheet-details"
 import Home from "../components/home"
@@ -7,16 +7,14 @@ import Home from "../components/home"
 
 // TODO: Loading all pages at once can perform badly for huge libraries, fix with LazyLoad vs offline mode?
 export default function Sheetlist({ data }) {
-  const [searchString, setSearchString] = useState("")
 
   const sheetlist = data.allMarkdownRemark.nodes
-  sheetlist.sort((a,b) => a.frontmatter.title.localeCompare(b.frontmatter.title))
   const NotFound = () => (<div onLoad={navigate("/", {replace: true})}>Page not found</div>)
 
 
   return (
     <Router>
-      <Home sheetlist={sheetlist} searchString={searchString} setSearchString={setSearchString} path="/" />
+      <Home sheetlist={sheetlist} path="/" />
       {sheetlist.map(sheet => (
         <SheetPage data={sheet} path={`/sheets/${sheet.frontmatter.slug}`} />
       ))}
